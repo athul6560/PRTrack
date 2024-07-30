16950c2c04ae6d83a30a43727bdcb292d62d557f
athul <zeeza.global@gmail.com>
2024-07-30 14:52:12 -0400

    graph implemented

--- app/src/main/java/com/zeezaglobal/prtrack/MainActivity.kt
+++ app/src/main/java/com/zeezaglobal/prtrack/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.zeezaglobal.prtrack
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.zeezaglobal.prtrack.Vies.createLineChartView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val parentLayout = findViewById<LinearLayout>(R.id.chartContainer)
+
+        createLineChartView(
+            context = this,
+            parent = parentLayout,
+            dataPoints = listOf(
+                Pair("Jan", 10f),
+                Pair("Feb", 20f),
+                Pair("Mar", 30f),
+                Pair("Apr", 40f),
+                Pair("May", 50f)
+            ),
+            yLabels = listOf("0kg", "20kg", "40kg", "60kg"),
+            xAxisColor = Color.RED,
+            yAxisColor = Color.GREEN,
+            gridColor = Color.GRAY,
+            lineColor = Color.MAGENTA,
+            maxDataPointY = 60f
+        )
     }
-}
+    }
+
