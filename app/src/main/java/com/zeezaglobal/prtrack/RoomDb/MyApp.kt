cdfbf68022f714177c74abcbdbe8e077380ad093
Athul Babu <athul.6560@gmail.com>
2024-09-18 08:59:29 -0400

    add workout optimised

--- app/src/main/java/com/zeezaglobal/prtrack/RoomDb/MyApp.kt
+++ app/src/main/java/com/zeezaglobal/prtrack/RoomDb/MyApp.kt
@@ -1,6 +1,7 @@
 package com.zeezaglobal.prtrack.RoomDb
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
 import androidx.room.Room
 import com.zeezaglobal.prtrack.Utils.populateDatabase
@@ -16,9 +17,25 @@ class MyApp : Application() {
             "my-database"
         ).build()
 
-        val bodyPartDao = database.bodyPartDao()
 
-        // Populate the database with dummy data
-       // populateDatabase(bodyPartDao)
-    }
+
+        // Get SharedPreferences
+        val sharedPreferences = getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE)
+
+        // Check if the database has been populated before
+        val isDatabasePopulated = sharedPreferences.getBoolean("isDatabasePopulated", false)
+
+        if (!isDatabasePopulated) {
+            // Populate the database with dummy data
+            val bodyPartDao = database.bodyPartDao()
+            populateDatabase(bodyPartDao)
+
+            // Set the flag to true so it won't run again
+            sharedPreferences.edit().putBoolean("isDatabasePopulated", true).apply()
+
+            Log.d("MyApp", "Database populated with initial data")
+        } else {
+            Log.d("MyApp", "Database population skipped")
+        }
+        }
 }
